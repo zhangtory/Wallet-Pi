d9b0532300af2fb588b0910a3d1543f403765324
scott <scott@flashdot.com>
2022-12-31 19:43:36 +0800

    update.

--- wallet/wallet-core/wallet-core-spring-boot-autoconfigure/target/classes/db/schema-h2.sql
+++ wallet/wallet-core/wallet-core-spring-boot-autoconfigure/target/classes/db/schema-h2.sql
@@ -1,6 +1,6 @@
 
-drop table if exists mnemonic;
-CREATE TABLE mnemonic (
+drop table if exists wallet;
+CREATE TABLE wallet (
   id int primary key AUTO_INCREMENT,
   mnemonics varchar(1024),
   create_time datetime NOT NULL DEFAULT current_timestamp(),
